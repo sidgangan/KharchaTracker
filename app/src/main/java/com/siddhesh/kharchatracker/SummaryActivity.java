7f58c1a26c2f71acab0f36e039850ed6e11c6b59
sidgangan <sgangan298@gmail.com>
2019-02-02 22:04:44 +0530

    added bottom navigation bar and created new fragments

--- app/src/main/java/com/siddhesh/kharchatracker/SummaryActivity.java
+++ app/src/main/java/com/siddhesh/kharchatracker/SummaryActivity.java
@@ -1,13 +1,61 @@
 package com.siddhesh.kharchatracker;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class SummaryActivity extends AppCompatActivity {
+
+    private Drawable icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(new SummaryFragment());
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+
+            switch (item.getItemId()) {
+                case R.id.summary:
+                    fragment = new SummaryFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.expenses:
+                    fragment = new ExpensesFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.add:
+                    fragment = new AddFragment();
+                    loadFragment(fragment);
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
